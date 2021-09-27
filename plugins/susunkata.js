472ce8e57caef0f43495334d75acaf0e7d7e803d
bulanbintang69 <87313549+bulanbintang69@users.noreply.github.com>
2021-09-28 01:44:28 +0700

    Update susunkata.js

--- plugins/susunkata.js
+++ plugins/susunkata.js
@@ -26,7 +26,7 @@ Bonus: ${poin} XP
         await conn.sendButton(m.chat, caption, 'https://t.me/whatsappbot1', 'Bantuan', '.suka'),
         json, poin,
         setTimeout(async () => {
-            if (conn.susunkata[id]) await conn.sendButton(m.chat, `Waktu habis!\nJawabannya adalah *${json.jawaban}*`, '© stikerin', 'Susun Kata', '.susunkata')
+            if (conn.susunkata[id]) await conn.sendButton(m.chat, `Waktu habis!\nJawabannya adalah *${json.jawaban}*`, '♕Bot sky♕', 'Susun Kata', '.susunkata')
             delete conn.susunkata[id]
         }, timeout)
     ]
