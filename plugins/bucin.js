88419881c3cfcf0fc85d4ad151115bf469619ea7
bulanbintang69 <87313549+bulanbintang69@users.noreply.github.com>
2021-09-28 00:54:38 +0700

    Update bucin.js

--- plugins/bucin.js
+++ plugins/bucin.js
@@ -1,5 +1,5 @@
 let handler = async (m, { conn, usedPrefix, command }) => {
-  await conn.sendButton(m.chat, `“${pickRandom(global.bucin)}”`, '© stikerin', 'Bucin', `${usedPrefix + command}`)
+  await conn.sendButton(m.chat, `“${pickRandom(global.bucin)}”`, 'https://t.me/whatsappbot1', 'Bucin', `${usedPrefix + command}`)
 }
 handler.help = ['bucin']
 handler.tags = ['quotes']
