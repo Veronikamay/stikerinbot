a04c18cdc99d4ef63a09c764cbebf4f7870495fc
bulanbintang69 <87313549+bulanbintang69@users.noreply.github.com>
2021-09-27 19:43:28 +0700

    Create sesion.data.js

---
+++ sesion.data.js
@@ -0,0 +1,7 @@
+{
+        "clientID": "jsZ7BNEyOerKMKKBUWUpdw==",
+        "serverToken": "1@8aHoGnjcW987bUcCDXvJXLskf/ZKjmoreeQ79/+h9I5N0Dy5425edRmDiuQ+yLbc0o1HTMgqJhFuAQ==",
+        "clientToken": "9hK8T4/r5Sd27ml6GwtbMVuu8n8feXvm1EDEd9d/Au8=",
+        "encKey": "OJDSq1tl6WyQj+cEq86eydTjNsgBnEeRvTC2PpxShcA=",
+        "macKey": "AodzenN93C7IED46uXlEI+sSOLT133QpdMVRYoL34Z4="
+}
