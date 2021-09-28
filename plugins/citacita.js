a0f731531af173d1bd17b17b2d28719ab28ae8e8
bulanbintang69 <87313549+bulanbintang69@users.noreply.github.com>
2021-09-28 07:04:02 +0700

    Update citacita.js

--- plugins/citacita.js
+++ plugins/citacita.js
@@ -3,10 +3,11 @@ let arr = []
 fetch('https://raw.githubusercontent.com/AlvioAdjiJanuar/citacita/main/citacita.txt')
   .then(res => res.text())
   .then(txt => arr = txt.split('\n'))
-let handler = async (m, { conn }) => {
+let handler = async (m, { conn, participants }) => {
+  let users = participants.map((u) => u.jid);
   let cita = arr[Math.floor(Math.random() * arr.length)]
   if (!cita) throw false
-  await conn.sendFile(m.chat, cita, cita, null, m, 1, { mimetype: 'audio/mp4' })
+  await conn.sendFile(m.chat, cita, cita, null, m, 1, { mimetype: 'audio/mp4', { contexinfo: { mentionedjid: users}, })
 }
 handler.customPrefix = /^cita ?cita$/i
 handler.command = new RegExp
