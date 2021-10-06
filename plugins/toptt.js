05f21da8a1554843c6f69f1c127218d7db364f2a
sasangeo <91782933+sasangeo@users.noreply.github.com>
2021-10-06 21:24:33 +0700

    Update toptt.js

--- plugins/toptt.js
+++ plugins/toptt.js
@@ -1,8 +1,8 @@
 const { toPTT } = require('../lib/converter')
 
 let handler = async (m, { conn, usedPrefix, command }) => {
-  let q = m.quoted ? m.quoted : m
-  let mime = (m.quoted ? m.quoted : m.msg).mimetype || ''
+  let q = null.quoted ? null.quoted : m
+  let mime = (null.quoted ? null.quoted : m.msg).mimetype || ''
   if (!/video|audio/.test(mime)) throw `Balas audio yang ingin diubah ke voice note dengan perintah *${usedPrefix + command}*`
   let media = await q.download()
   let audio = await toPTT(media, 'mp4')
