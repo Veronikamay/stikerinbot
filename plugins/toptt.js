ad6ab2b2d6b39eb95fcdcbdbe3c84aeec598bc48
sasangeo <91782933+sasangeo@users.noreply.github.com>
2021-10-06 21:52:56 +0700

    Add files via upload

--- plugins/toptt.js
+++ plugins/toptt.js
@@ -2,12 +2,11 @@ const { toPTT } = require('../lib/converter')
 
 let handler = async (m, { conn, usedPrefix, command }) => {
   let q = m.quoted ? m.quoted : m
-  let mime = (m.quoted ? m.quoted : 
-              .msg).mimetype || ''
+  let mime = (m.quoted ? m.quoted : m.msg).mimetype || ''
   if (!/video|audio/.test(mime)) throw `Balas audio yang ingin diubah ke voice note dengan perintah *${usedPrefix + command}*`
   let media = await q.download()
   let audio = await toPTT(media, 'mp4')
-  conn.sendFile(m.chat, audio, '', '', null, 1, { mimetype: 'audio/mp4' })
+  conn.sendFile(m.chat, audio, '', '', m, 1, { mimetype: 'audio/mp4' })
 }
 handler.help = ['tovn']
 handler.tags = ['audio']
