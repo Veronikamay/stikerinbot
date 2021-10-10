b2b15a8b7d68803ea1d0cb898b6fd1e39b5e4d5e
panggil aku shifa <87982800+Veronikamay@users.noreply.github.com>
2021-10-10 23:58:52 +0700

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -120,7 +120,7 @@ let handler = async (m, { conn, usedPrefix: _p, args, command }) => {
   if (teks == 'jadibot') tags = {
     'jadibot': 'Jadi Bot'
   }
-  if (teks == 'jadibot') tags = {
+  if (teks == 'topupgame') tags = {
     'topupgame': '🎮ML&FF🎮'
   }
   if (teks == 'info') tags = {
@@ -384,7 +384,7 @@ let handler = async (m, { conn, usedPrefix: _p, args, command }) => {
       readmore: readMore
     }
     text = text.replace(new RegExp(`%(${Object.keys(replace).sort((a, b) => b.length - a.length).join`|`})`, 'g'), (_, name) => '' + replace[name])
-    await conn.send2ButtonLoc(m.chat, await (await fetch("https://telegra.ph/file/e750471403ca953850c48.jpg")).buffer(), text.trim(), 'beri jeda bot 6 detik', 'Pemilik Bot', `${_p} owner`, 'Menu', `${_p} all`, m)
+    await conn.send2ButtonLoc(m.chat, await (await fetch("https://telegra.ph/file/e750471403ca953850c48.jpg")).buffer(), text.trim(), 'beri jeda bot 6 detik', 'Pemilik Bot', `${_p} owner`, 'Menu', `${_p} menu`, m)
   } catch (e) {
     conn.reply(m.chat, 'Maaf, menu sedang error', m)
     throw e
