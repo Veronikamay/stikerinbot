5e725f22b99b503dda52a2c638721db1f5dd6557
panggil aku shifa <87982800+Veronikamay@users.noreply.github.com>
2021-10-13 08:14:25 +0700

    Update topupdone.js

--- plugins/topupdone.js
+++ plugins/topupdone.js
@@ -1,5 +1,6 @@
 let fetch = require('node-fetch')
 let handler = async (m, { conn }) => await conn.sendButtonLoc(m.chat, await (await fetch("https://telegra.ph/file/22a4bfab48d72117ffa51.jpg")).buffer(), `
+servers: status diamod terkirim
 `.trim(), 'Terimakasih sudah menjadi bagian dari kami, Semoga hari ini menjadi hari yang indah bagi kita Semua. :)', 'Menu', '.menu', m)
 handler.help = ['done']
 handler.tags = ['topupgame']
