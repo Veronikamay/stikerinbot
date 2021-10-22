5ab0862069871cc4e96e3fc60f5b5ecc46a07f88
panggil aku shifa <87982800+Veronikamay@users.noreply.github.com>
2021-10-22 10:52:49 +0700

    Update _antilink.js

--- plugins/_antilink.js
+++ plugins/_antilink.js
@@ -1,37 +1,27 @@
 let handler = m => m
 
-let linkRegex = /chat.whatsapp.com\/(?:invite\/)?([0-9A-Za-z]{20,24})/i
+const isValidURL = (url) => {
+  return /(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%.\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%\+.~#?&//=]*)/g.test(url);
+}
 
 handler.before = async function (m, { isAdmin, isBotAdmin, command }) {
-
   if (m.isBaileys && m.fromMe) return true
-
   let chat = global.db.data.chats[m.chat];
-
-  let ValidLink = (m.text.includes('https://') || m.text.includes('http://'))
-
+  let ValidLink = isValidURL(m.text) == true && (prefix).test(m.text) == false;
   if (chat.antiLink && ValidLink && !isAdmin && !m.isBaileys && m.isGroup && !command) {
-
     let thisGroup = isBotAdmin ? `https://chat.whatsapp.com/${await conn.groupInviteCode(m.chat)}` : 0
-
     if (m.text.includes(thisGroup) && thisGroup != 0) throw false // jika link grup itu sendiri gak dikick
-
     await this.reply(global.owner[0] + '@s.whatsapp.net', `
 
-Pelaku pengirim troli @${m.sender.split`@`[0]}
+Pelaku Pengirim Link @${m.sender.split`@`[0]}
 
 ID: ${m.isGroup ? m.chat : m.sender}
 
 Nama: ${m.isGroup ? this.getName(m.chat) : this.getName(m.sender)}
 
 `.trim(), null, { contextInfo: { mentionedJid: [m.sender] } })
-
-    }
-
   }
-
   return true
-
 }
 
 module.exports = handler
