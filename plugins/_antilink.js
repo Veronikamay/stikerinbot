f2cadc384a6ea66b12f901d3013be645d1694c7e
panggil aku shifa <87982800+Veronikamay@users.noreply.github.com>
2021-10-22 07:36:08 +0700

    Update _antilink.js

--- plugins/_antilink.js
+++ plugins/_antilink.js
@@ -8,7 +8,7 @@ handler.before = async function (m, { isAdmin, isBotAdmin, command }) {
   if (chat.antiLink && ValidLink && !isAdmin && !m.isBaileys && m.isGroup && !command) {
     let thisGroup = isBotAdmin ? `https://chat.whatsapp.com/${await conn.groupInviteCode(m.chat)}` : 0
     if (m.text.includes(thisGroup) && thisGroup != 0) throw false // jika link grup itu sendiri gak dikick
-    await this.sendButton(m.chat, `*Link Terdeteksi!*${isBotAdmin ? '' : '\n\nbukan admin jadi gabisa kick t_t'}${opts['restrict'] ? '' : ''}`, '© bot', 'Matikan Antilink', ',0 antilink', m)
+    await this.sendButton(m.chat, `*Link Terdeteksi!*${isBotAdmin ? '' : '\n\nbukan admin jadi gabisa kick t_t'}${opts['restrict'] ? '' : ''}`, '© bot', 'Matikan Antilink', ',.report bang ada yang ngirim link', m)
     if (global.opts['restrict']) {
       if (isBotAdmin) this.groupRemove(m.chat, [m.sender])
     }
