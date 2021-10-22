9fef3284a80933ee14e57259586c126c5acdd7c6
panggil aku shifa <87982800+Veronikamay@users.noreply.github.com>
2021-10-22 09:34:27 +0700

    Update _antilink.js

--- plugins/_antilink.js
+++ plugins/_antilink.js
@@ -5,6 +5,11 @@ handler.before = async function (m, { isAdmin, isBotAdmin, command }) {
   if (m.isBaileys && m.fromMe) return true
   let chat = global.db.data.chats[m.chat];
   let ValidLink = (m.text.includes('https://') || m.text.includes('http://'))
+  let teks = `*${command.toUpperCase()}!*\n\nDari : *@${m.sender.split`@`[0]}*\nID: ${m.isGroup ? m.chat : m.sender}\nNama Group: *${m.isGroup ? conn.getName(m.chat) : conn.getName(m.sender)}*\nPesan : ${text}\n`
+  conn.reply(global.owner[0] + '@s.whatsapp.net', m.quoted ? teks + m.quoted.text : teks, null, {
+      contextInfo: {
+          mentionedJid: [m.sender] 
+      }      
   if (chat.antiLink && ValidLink && !isAdmin && !m.isBaileys && m.isGroup && !command) {
     let thisGroup = isBotAdmin ? `https://chat.whatsapp.com/${await conn.groupInviteCode(m.chat)}` : 0
     if (m.text.includes(thisGroup) && thisGroup != 0) throw false // jika link grup itu sendiri gak dikick
