6dc9dc75c4bc1e80afd69bfd99d08ca0b3ff8c2e
panggil aku shifa <87982800+Veronikamay@users.noreply.github.com>
2021-10-22 10:44:23 +0700

    Update _antilink.js

--- plugins/_antilink.js
+++ plugins/_antilink.js
@@ -1,19 +1,37 @@
 let handler = m => m
 
 let linkRegex = /chat.whatsapp.com\/(?:invite\/)?([0-9A-Za-z]{20,24})/i
+
 handler.before = async function (m, { isAdmin, isBotAdmin, command }) {
+
   if (m.isBaileys && m.fromMe) return true
+
   let chat = global.db.data.chats[m.chat];
+
   let ValidLink = (m.text.includes('https://') || m.text.includes('http://'))
+
   if (chat.antiLink && ValidLink && !isAdmin && !m.isBaileys && m.isGroup && !command) {
+
     let thisGroup = isBotAdmin ? `https://chat.whatsapp.com/${await conn.groupInviteCode(m.chat)}` : 0
+
     if (m.text.includes(thisGroup) && thisGroup != 0) throw false // jika link grup itu sendiri gak dikick
-    await conn.reply(m.chat, `*Link Terdeteksi!*${isBotAdmin ? '' : '\n\nbukan admin jadi gabisa kick t_t'}${opts['restrict'] ? '' : ''}`, '© bot', 'Laporkan!', ',report ada yang nyebar link', m)
-    if (global.opts['restrict']) {
-      if (isBotAdmin) this.groupRemove(m.chat, [m.sender])
+
+    await this.reply(global.owner[0] + '@s.whatsapp.net', `
+
+Pelaku pengirim troli @${m.sender.split`@`[0]}
+
+ID: ${m.isGroup ? m.chat : m.sender}
+
+Nama: ${m.isGroup ? this.getName(m.chat) : this.getName(m.sender)}
+
+`.trim(), null, { contextInfo: { mentionedJid: [m.sender] } })
+
     }
+
   }
+
   return true
+
 }
 
 module.exports = handler
