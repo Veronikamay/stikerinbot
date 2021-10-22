cca22973b10e79c1a0b98725ba623ddef759e3da
panggil aku shifa <87982800+Veronikamay@users.noreply.github.com>
2021-10-22 07:19:52 +0700

    Update broadcastgroups.js

--- plugins/broadcastgroups.js
+++ plugins/broadcastgroups.js
@@ -5,9 +5,9 @@ let handler = async (m, { conn, text }) => {
   conn.reply(m.chat, `_Mengirim pesan broadcast ke ${groups.length} grup_\nestimasi selesai ${groups.length * 1.5} detik`, m)
   for (let id of groups) {
     await delay(1500)
-    await conn.copyNForward(id, conn.cMod(m.chat, cc, /bc|broadcast/i.test(teks) ? teks : '〔 Broadcast 〕\n\n' + teks + '\n\n' + '♕»»——⍟——««♕'), false).catch(_ => _)
+    await conn.copyNForward(id, conn.cMod(m.chat, cc, /bc|broadcast/i.test(teks) ? teks : '»»—Broadcast—««\n\n' + teks + '\n\n' + '    »»——⍟——««'), false).catch(_ => _)
   }
-  m.reply('_*Broadcast Selesai*_')
+  m.reply('_*»»—Broadcast⍟Selesai—««*_')
 }
 handler.help = ['broadcastgroup', 'bcgc'].map(v => v + ' <teks>')
 handler.tags = ['owner']
