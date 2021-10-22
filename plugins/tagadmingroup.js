d27f1c790ab090eb68c7c93e522abca350d01546
panggil aku shifa <87982800+Veronikamay@users.noreply.github.com>
2021-10-22 22:11:43 +0700

    Create tagadmingroup.js

---
+++ plugins/tagadmingroup.js
@@ -0,0 +1,51 @@
+let handler = async (m, { conn, participants, groupMetadata, text }) => {
+
+    const getGroupAdmins = (participants) => {
+        admins = []
+        for (let i of participants) {
+            i.isAdmin ? admins.push(i.jid) : ''
+        }
+        return admins
+    }
+
+    let pp = ''
+    try {
+        pp = 
+    } catch (e) {
+    } finally {
+        let { isBanned, welcome, detect, sWelcome, sBye, sPromote, sDemote, antiLink, expired, descUpdate, stiker } = global.db.data.chats[m.chat]
+        const groupAdmins = getGroupAdmins(participants)
+        let listAdmin = groupAdmins.map((v, i) => `${i + 1}. @${v.split`@`[0]}`).join('\n')
+
+        if (text) return m.reply(msToDate(expired - new Date() * 1))
+
+        let caption = `Tag Admin\n
+*Pembuat Grup:*
+@${m.chat.split`-`[0]}
+*Admin Grup:*
+${listAdmin}
+
+`.trim()
+        let mentionedJid = groupAdmins.concat([`${m.chat.split`-`[0]}@s.whatsapp.net`])
+    }
+}
+handler.help = ['admingroup']
+handler.tags = ['group']
+handler.command = /^(group?admin|admin(gro?up|gc))$/i
+
+handler.group = true
+
+module.exports = handler
+
+function msToDate(ms) {
+    temp = ms
+    days = Math.floor(ms / (24 * 60 * 60 * 1000));
+    daysms = ms % (24 * 60 * 60 * 1000);
+    hours = Math.floor((daysms) / (60 * 60 * 1000));
+    hoursms = ms % (60 * 60 * 1000);
+    minutes = Math.floor((hoursms) / (60 * 1000));
+    minutesms = ms % (60 * 1000);
+    sec = Math.floor((minutesms) / (1000));
+    return days + " hari " + hours + " jam " + minutes + " menit";
+    // +minutes+":"+sec;
+}
